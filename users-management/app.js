f5ff7798f7c12ad17d99e117e0e96bea58c0866a
ranik20 <ranik20@gmail.com>
2026-05-28 20:50:45 +0300

    new porject nodejs and mongodb

---
+++ users-management/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;  
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log('Press Ctrl+C to stop the server.');
+  console.log('Use the following endpoint to get the list of users:');
+  console.log(`App is running on !!!!! ${port}`);
+  console.log(`hello world`);
+});
+
+app.get('/', (req, res) => {
+  const users = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }
+  ];
+  res.json(users);
+});
